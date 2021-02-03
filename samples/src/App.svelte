874119e1726e927b06a7f99d4142adec11e6faeb
Shirish Nigam <nigamshirish@gmail.com>
2021-02-03 21:39:52 +1000

    Rename sample folder to samples

---
+++ samples/src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  // CKEditor wrapper component
+  import CKEditor from "../../dist/index.js";
+  // Demonstrated usage of Decoupled Editor Build
+  import DecoupledEditor from "@ckeditor/ckeditor5-build-decoupled-document/build/ckeditor";
+
+  // Setting up editor prop to be sent to wrapper component
+  let editor = DecoupledEditor;
+  // Reference to initialised editor instance
+  let editorInstance = null;
+  // Setting up any initial data for the editor
+  let editorData = "Hello World";
+
+  // If needed, custom editor config can be passed through to the component
+  // Uncomment the custom editor config if you need to customise the editor.
+  let editorConfig = {
+    // toolbar: {
+    //   items: [
+    //     "heading",
+    //     "|",
+    //     "fontFamily",
+    //     "fontSize",
+    //     "bold",
+    //     "italic",
+    //     "underline"
+    //   ]
+    // }
+  };
+
+  function onReady({ detail: editor }) {
+    // Insert the toolbar before the editable area.
+    editorInstance = editor;
+    editor.ui
+      .getEditableElement()
+      .parentElement.insertBefore(
+        editor.ui.view.toolbar.element,
+        editor.ui.getEditableElement()
+      );
+  }
+</script>
+
+<div>
+  <CKEditor
+    bind:editor
+    on:ready={onReady}
+    bind:config={editorConfig}
+    bind:value={editorData} />
+</div>

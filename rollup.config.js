bc5fd31965b38c188a3150949c61804bc56043ad
Shirish Nigam <nigamshirish@gmail.com>
2021-02-03 21:36:27 +1000

    Upgrade package dev dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import svelte from "rollup-plugin-svelte"
-import resolve from "rollup-plugin-node-resolve"
-import commonjs from "rollup-plugin-commonjs"
-import pkg from "./package.json"
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
 
 export default {
-  input: "src/Ckeditor.svelte",
+  input: 'src/Ckeditor.svelte',
   output: [
-    { file: pkg.module, format: "es" },
-    { file: pkg.main, format: "umd", name: "Ckeditor" }
+    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'umd', name: 'Ckeditor' },
   ],
-  plugins: [svelte(), resolve(), commonjs()]
-}
+  plugins: [svelte(), resolve(), commonjs()],
+};
